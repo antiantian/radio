53a8e37503f9610fd4e8b989ab92bbb42c42d18e
anyuer <1366969408@qq.com>
2017-04-07 09:54:10 +0800

    Update radio.js

--- example/comon/radio.js
+++ example/comon/radio.js
@@ -12,7 +12,7 @@ import {
 var Dimensions = require('Dimensions');
 var width=Dimensions.get('window').width;
 var height=Dimensions.get('window').height;
-import RadioModal from 'react-native-radio-model';
+import RadioModal from 'react-native-radio-master';
 var datas= [
             {
               "selecteId": 13,
@@ -77,19 +77,14 @@ export default class RadioModalItem extends Component{
 							  flex:1,
 							  backgroundColor:'#ffffff',padding:5,marginTop:10
 							  }} 
-						>
-						  <Text value="0">选项a</Text>
-						  <Text value="1" disabled="true">选项b</Text>
-						  <Text value="2">选项c</Text>
-						  <Text value="3">选项d</Text>
-					</RadioModal>
-					<Text style={{backgroundColor:'#ffffff',color:'#414141',padding:5,}}>
-				     The selected:<Text style={{color:'#ff0000'}}>{this.state.initItem}</Text>	 
-				    </Text>	 
-				    <Text style={{backgroundColor:'#ffffff',color:'#414141',padding:5,}}>	 
-                     Unique identification：<Text style={{color:'#ff0000'}}>{this.state.initId}</Text>
-				    </Text>
-		           <RadioModal
+				      />
+				     <Text style={{backgroundColor:'#ffffff',color:'#414141',padding:5,}}>
+				            The selected:<Text style={{color:'#ff0000'}}>{this.state.initItem}</Text>	 
+				      </Text>	 
+				      <Text style={{backgroundColor:'#ffffff',color:'#414141',padding:5,}}>	 
+                                            Unique identification：<Text style={{color:'#ff0000'}}>{this.state.initId}</Text>
+				      </Text>
+		                      <RadioModal
 						selectedValue={this.state.initId}
 						onValueChange={(id,item) => this.setState({initId: id,initItem:item})}
 						style={{ flexDirection:'row',
@@ -103,7 +98,7 @@ export default class RadioModalItem extends Component{
 						  <Text value="1" disabled="true">选项b</Text>
 						  <Text value="2">选项c</Text>
 						  <Text value="3">选项d</Text>
-					</RadioModal>
+				       </RadioModal>
 				</View>
 
 		);
